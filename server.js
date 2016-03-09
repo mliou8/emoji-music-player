c5ed3be3eb63da7d135d336d624b6685c7e122dd
mliou8 <chunghanliou@gmail.com>
2016-03-09 13:00:32 -0500

    Initializing repo

---
+++ server.js
@@ -0,0 +1,22 @@
+var path = require('path');
+
+var http = require('http');
+var server = http.createServer();
+
+var express = require('express');
+var app = express();
+
+server.on('request', app);
+
+server.listen(3000, function() {
+  console.log('The server is listening on port 3000!');
+});
+
+app.use(express.static(path.join(__dirname, 'browser')));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use('/bower_components', express.static(__dirname + '/bower_components'));
+
+
+app.get('/', function(req, res) {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
