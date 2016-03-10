feee6a2468089654f877ade02a0ee5a8fb915b6b
mliou8 <chunghanliou@gmail.com>
2016-03-10 03:06:37 -0500

    ng repeating images and they can snap on without data

--- public/js/controllers/mainCtrl.js
+++ public/js/controllers/mainCtrl.js
@@ -1,3 +1,28 @@
 app.controller('mainCtrl', function($scope) {
-  $scope.hey = "hey";
+  $scope.list1 = "content";
+  $scope.list2 = "";
+  $scope.images = [{
+    id: "bearDiv",
+    src: "/images/bear.png"
+  }, {
+    id: "santaDiv",
+    src: "/images/santa_claus.png"
+  }, {
+    id: "turtleDiv",
+    src: "/images/turtle.png"
+  }, {
+    id: "volcanoDiv",
+    src: "/images/volcano.png"
+  }, {
+    id: "devilDiv",
+    src: "/images/devil.png"
+  }, {
+    id: "injuredDiv",
+    src: "/images/injured.png"
+  }];
+
+  $scope.play = function() {
+    console.log("$scope.list1 ", $scope.list1);
+    console.log("$scope.list2 ", $scope.list2);
+  }
 })
