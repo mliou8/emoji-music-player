e1756a78df53f34c99820d4df744c7de2e86275d
mliou8 <chunghanliou@gmail.com>
2016-03-12 15:15:17 -0500

    adding better pagination for columns of emojis

--- README.md
+++ README.md
@@ -1,18 +1,24 @@
 # emoji-music-player
 
--This is an app where you drag emojis around on a screen. Then when you hit a "render"		
--button, it will render a certain spotify song based on what you moved into the box.		
--		
--** Still in Progress **		
--		
--To-dos:		
--		
---- Render a "board" that the emojis can snap onto.		
---- Calculate a "code" based on which emojis are where.		
---- Plug in Spotify API so that you can pull back previews of a certain song, and render that player somewhere too.		
--		
--		
--		
+- This is an app where you drag emojis around on a screen. Then when you hit a "render" button, it will render a certain spotify song based on what you moved into the box.		
+
+** Still in Progress **		
+
+To-dos:		
+-- List of songs, at least 10 songs that all have a value split (key corresponding)
+-- Calculate positions and generate a song based on that.
+-- Fix up styling
+-- Deploy!
+
+
+Done:
+
+--- Render a "board" that the emojis can snap onto.		Done
+--- Calculate a "code" based on which emojis are where.	Done
+--- Plug in Spotify API so that you can pull back previews of a certain song, and render that player somewhere too.		Done
+
+
 -Bonuses:		
 --- Board is a background with colors		
 --- Can swap between different boards
+-- D3 Visualization of emojis
--- public/js/controllers/mainCtrl.js
+++ public/js/controllers/mainCtrl.js
@@ -1,6 +1,9 @@
-app.controller('mainCtrl', function($scope) {
+app.controller('mainCtrl', function($scope, $sce) {
 
-  $scope.images = [{
+  $scope.showPlayer = false;
+  var score = 0;
+
+  var images = [{
     id: "bearDiv",
     src: "/images/bear.png"
   }, {
@@ -18,10 +21,82 @@ app.controller('mainCtrl', function($scope) {
   }, {
     id: "injuredDiv",
     src: "/images/injured.png"
+  }, {
+    id: "chineseDiv",
+    src: "/images/chinese.png"
+  }, {
+    id: "yinyangDiv",
+    src: "/images/yinyang.png"
+  }, {
+    id: "fireDiv",
+    src: "/images/firetruck.png"
+  }, {
+    id: "angryDiv",
+    src: "/images/angry.png"
+  }, {
+    id: "toiletDiv",
+    src: "/images/toilet.png"
   }];
 
+  //Sort NG-repeat into columns
+  function chunk(arr, size) {
+    var newArr = [];
+    for (var i = 0; i < arr.length; i += size) {
+      newArr.push(arr.slice(i, i + size));
+    }
+    return newArr;
+  }
+
+  $scope.images = chunk(images, 4);
+  console.log("$scope.images ", $scope.images)
+
+  //List of Spotify URIS
+  $scope.music = [{
+    url: "https://embed.spotify.com/?uri=spotify:track:3VRXedC3QOvXZATRGlAyDR", //Nicki Minaj Song
+    value: 10
+  }, {
+    url: "https://embed.spotify.com/?uri=spotify:track:0rkajHs0Kcw73nFDlKmuBk", //Classical Beethoven
+    value: 20
+  }, {
+    url: "https://embed.spotify.com/?uri=spotify:track:13uW9hmN28qYQqoPykMB5d", // Summer Time
+    value: 30
+  }, {
+    url: "https://embed.spotify.com/?uri=spotify:track:2qdP4opHhLkDUGxzirBy8M", // Skinny Love
+    value: 40
+  }, {
+    url: "https://embed.spotify.com/?uri=spotify:track:7HzCxalzzYQOFb9a7Xs3j6", //Ibiza
+    value: 50
+  }, {
+    url: "https://embed.spotify.com/?uri=spotify:track:0IKK48xF4eEdfofyaeKWWO", //Pillowtalk
+    value: 60
+  }]
+
+  function songUpdate() {
+    $scope.musicGenerator.forEach(function(data) {
+      score += (data.position[0] + data.position[1])
+    })
+    score = Math.ceil((Math.random() * 100) / 10) * 10;
+    for (var i = 0; i < $scope.music.length; i++) {
+      if (score === $scope.music[i].value) {
+        $scope.musicUrl = $scope.music[i].url
+      }
+    }
+  }
+
+  $scope.generate = function() {
+    songUpdate();
+    $scope.musicUrl = $sce.trustAsResourceUrl($scope.musicUrl);
+    $scope.showPlayer = true;
+  }
+
+  $scope.clear = function() {
+    $scope.showPlayer = false;
+  }
+
+  //Track statistics of where things are dropped
   $scope.musicGenerator = [];
 
+  // Updating
   $scope.update = function(e) {
     var found = $scope.musicGenerator.some(function(el) {
       return el.name === e.target.id;
@@ -39,7 +114,5 @@ app.controller('mainCtrl', function($scope) {
     }
   }
 
-  $scope.play = function() {
-    console.log($scope.musicGenerator);
-  }
+
 })
