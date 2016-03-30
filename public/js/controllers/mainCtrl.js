e787cd4d0affc7d242caded4a937a590ac896537
mliou8 <chunghanliou@gmail.com>
2016-03-30 18:52:27 -0400

    adding a simplified working algorithm

--- public/js/controllers/mainCtrl.js
+++ public/js/controllers/mainCtrl.js
@@ -1,14 +1,14 @@
 app.controller('mainCtrl', function($scope, $sce, emojiFactory, musicFactory) {
 
   $scope.showPlayer = false;
-  var score = {
-    sad_score: 0,
-    fire_score: 0,
-    angry_score: 0,
-    top10_score: 0,
-    chill_score: 0
-  };
-
+  //temp storage for valid songs
+  var valid_songs = [];
+  //final song URL
+  $scope.musicUrl = "";
+  //List of Spotify URIS
+  $scope.music = musicFactory.songs;
+  //Track statistics of where things are dropped
+  var boardEmojis = [];
 
   //Sort NG-repeat into columns
   function chunk(arr, size) {
@@ -18,53 +18,13 @@ app.controller('mainCtrl', function($scope, $sce, emojiFactory, musicFactory) {
     }
     return newArr;
   }
-
   var images = emojiFactory.images;
   $scope.images = chunk(images, 4);
-
-  $scope.musicUrl = "";
-
-  // Helper Function to pick the song
-  function pickSong() {
-    $scope.musicGenerator.forEach(function(emoji) {
-      for (var key in score) {
-        if (emoji[key]) {
-          //apply the modifier to the score
-          score[key] = score[key] + (emoji.position[0] + emoji.position[
-            1]) * emoji[key]
-        }
-      }
-    })
-    console.log("score ", score);
-    for (var i = 0; i < $scope.music.length; i++) {
-      if (score === $scope.music[i].value) {
-        $scope.musicUrl = $scope.music[i].url
-      }
-    }
-  }
-
-  //Generate instance of spotify player
-  $scope.generate = function() {
-    pickSong();
-    console.log("$scope.musicGenerator ", $scope.musicGenerator);
-    $scope.musicUrl = $sce.trustAsResourceUrl($scope.musicUrl);
-    $scope.showPlayer = true;
-  }
-
-  $scope.clear = function() {
-    $scope.showPlayer = false;
-  }
-
-  //List of Spotify URIS
-  $scope.music = musicFactory.songs;
-
-  //Track statistics of where things are dropped
-  $scope.musicGenerator = [];
 
   // Updating the generator object
   $scope.updateGen = function(e) {
     //returns true or false
-    var found = $scope.musicGenerator.some(function(el) {
+    var found = boardEmojis.some(function(el) {
       return el.name === e.target.id;
     });
     if (!found) {
@@ -80,13 +40,42 @@ app.controller('mainCtrl', function($scope, $sce, emojiFactory, musicFactory) {
           }
         }
       }
-      $scope.musicGenerator.push(temp);
+      boardEmojis.push(temp);
     }
     //If it already exists update the position
-    for (var i = 0; i < $scope.musicGenerator.length; i++) {
-      if ($scope.musicGenerator[i].name === e.target.id) {
-        $scope.musicGenerator[i].position = [e.screenX, e.screenY];
+    for (var i = 0; i < boardEmojis.length; i++) {
+      if (boardEmojis[i].name === e.target.id) {
+        boardEmojis[i].position = [e.screenX, e.screenY];
       }
     }
+  }
+
+  // Helper Function to pick the song
+  function pickSong() {
+    $scope.music.forEach(function(song) {
+        for (var i = 0; i < boardEmojis.length; i++) {
+          for (var j = 0; j < song.emoji_keys.length; j++) {
+            if (boardEmojis[i].name.indexOf(song.emoji_keys[j]) !== -
+              1) {
+              //Match found
+              valid_songs.push(song.url);
+            }
+          }
+        }
+      })
+      //Finished populating valid_songs, return a valid song_url
+    var randIndex = Math.floor((Math.random() * valid_songs.length + 1));
+    $scope.musicUrl = valid_songs[randIndex];
+  }
+
+
+  //Generate instance of spotify player
+  $scope.generate = function() {
+    pickSong();
+    $scope.musicUrl = $sce.trustAsResourceUrl($scope.musicUrl);
+    $scope.showPlayer = true;
+  }
+  $scope.clear = function() {
+    $scope.showPlayer = false;
   }
 })
